e465b0c8aa1a3c5890119c83b8ee692af323ccf5
Armando Ramirez <armando.ramirez@atlatlgroup.com>
2018-03-28 19:08:42 -0600

    Se agrego el login a la ventana de universos

--- logincoroico/index.php
+++ logincoroico/index.php
@@ -46,9 +46,9 @@
   	<?php endif ?>
 
     <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php  if (isset($_SESSION['username'])) : 
+    	header("Location:public_html/principal.html")  ?>
+    <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
 		
